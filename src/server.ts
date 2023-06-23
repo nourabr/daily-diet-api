bd472e6fcba5e25a4d339e58447644ce515fa552
Noura <edcarlos4844@gmail.com>
2023-06-23 02:16:10 -0300

    server split between app.js and server.js

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
-import fastify from 'fastify'
-import { mealsRoutes } from './routes/meals'
-import cookie from '@fastify/cookie'
+import { app } from './app'
 
-const app = fastify()
-
-app.register(cookie)
 app
-  .register(mealsRoutes, {
-    prefix: '/meals',
-  })
-
   .listen({
     path: 'http://localhost',
     port: 3333,
