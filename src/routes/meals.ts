2760a598255710d5f930c571e34b2433ec0daebb
Noura <edcarlos4844@gmail.com>
2023-06-23 02:09:16 -0300

    Route: Create user

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,30 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { randomUUID } from 'crypto'
 
 export async function mealsRoutes(app: FastifyInstance) {
+  // Create a user using session ID
+  app.get('/create-user', (request, reply) => {
+    let { sessionId } = request.cookies
+
+    let feedback = 'Account created with success!'
+    let statusCode = 201
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+    } else {
+      feedback = `You are already logged in as: ${sessionId}`
+      statusCode = 200
+    }
+
+    reply.setCookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    })
+
+    return reply.status(statusCode).send(feedback)
+  })
+
   app.get('/', async (request, reply) => {
     const result = await knex('meals').select()
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import { mealsRoutes } from './routes/meals'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+app.register(cookie)
 app
   .register(mealsRoutes, {
     prefix: '/meals',
