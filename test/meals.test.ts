f6481b8dab0aba422a64f552a148118c0497ed6c
Noura <edcarlos4844@gmail.com>
2023-06-23 11:37:07 -0300

    e2e Test: Add a meal

--- test/meals.test.ts
+++ test/meals.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
 import { execSync } from 'child_process'
@@ -20,5 +20,25 @@ describe('Meals Route testing', () => {
   it('should be able to create a user', async () => {
     request(app.server).get('/meals/create-user').expect(201)
     request(app.server).get('/meals/create-user').expect(200)
+  })
+
+  it('should be able to add a meal', async () => {
+    const createUserRequest = request(app.server)
+      .get('/meals/create-user')
+      .expect(201)
+
+    const cookie = createUserRequest.get('setCookie')
+
+    request(app.server)
+      .post('/meals')
+      .set('Cookie', cookie)
+      .send({
+        name: 'Janta',
+        description: 'Arroz, feijão, ovos cozidos',
+        on_diet: true,
+      })
+      .expect(201)
+
+    expect.stringContaining('Meal added with sucess!')
   })
 })
